e56024b671254ee8520a4727fb3698f05a69c713
guest01 <guest01@ITELABD28>
2022-06-29 19:45:47 -0300

    29.26.22

---
+++ ExercíciosEntra21/CadastroCaminhao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastroCaminhao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
