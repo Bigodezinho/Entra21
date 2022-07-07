486b504963e819087a3ee935abd49b56f83b2a44
guest01 <guest01@ITELABD28>
2022-07-06 22:02:56 -0300

    06.07.22

---
+++ ExercíciosEntra21/RevisaoHerdar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RevisaoHerdar
+{
+    interface IPiloto : IPessoa
+    { 
+        public string Licenca { get; set; }
+    }
+    public class PilotoF1 : IPiloto
+    {
+        public string Licenca { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Nome { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Cpf { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+    public class Piloto : IPessoa
+    {
+        public string Nome { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+       public string Cpf { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    interface IPessoa
+    { 
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static List<PilotoF1> BuscarPiloto(List<PilotoF1> pilotoF1s, string cpf)
+            {
+                PilotoF1 pilotoF1s1 = new List<PilotoF1>();
+                for (int i = 0; i < pilotoF1s.Count; i++)
+                {
+                    if (pilotoF1s[i].Cpf.Contains(cpf))
+                    {
+                        pilotoF1s[i].Add(pilotoF1s[i]);
+                    }
+                }
+                return pilotoF1s1;
+            }
+        }
+    }
+}
